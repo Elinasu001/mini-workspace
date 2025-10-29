f139c6b5244505443356263b2730f97f7120e597
ajungeul93-rgb <ajungeul93@users.noreply.github.com>
2025-10-29 21:26:57 +0900

    VO 작업중

---
+++ CarTalkProject/src/main/java/com/kh/spring/used/model/vo/UsedListing.java
@@ -0,0 +1,14 @@
+package com.kh.spring.used.model.vo;
+
+public class UsedListing {
+	
+	private int usedNo;
+	private int userNo;
+	private String usedTitle;
+	private String usedContent;
+	private int usedPrice;
+	private String status;
+	private Date enrollDate;
+	private int viewCount;
+
+}
